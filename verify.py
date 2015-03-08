6f3240ddb0c29b7255595611ba9f91130a6437f7
Killian McCutcheon <entarter at gmail.com>
2015-03-08 13:17:11 +0900

    Assertion/reporting now abstracted to allow strategies and context tracking
    Validation is performed using an error reporter, which internally instantiates
    a checker. Checkers provide the interface for performing tests (asserts), and
    report any errors to the reporter. Checkers hold context information (what path
    is being checked), and decorate errors with this information.

--- verify.py
+++ verify.py
@@ -1,18 +1,16 @@
+import sys
 import types
 import json
-
-#from voluptuous import Schema, Required, All, Length, Range
-# 
-#schema = Schema({
-#    'types': '',
-#    }, extra=True)
 
 MAX_INT = (2**31) - 1
 MIN_INT = -MAX_INT - 1
 
 
-def extend_path(key, previous=None):
-    previous = '' if previous is None else (previous + '.')
+def extend_path(key, previous=''):
+    if isinstance(key, int):
+        key = '[%s]' % key
+    elif len(previous) > 0:
+        previous = previous + '.'
     return previous + key
 
 
@@ -21,17 +19,19 @@ class Doc:
         self.raw_data = data
         self.schema = schema
         self.indices = {}
-        schema.build_index(data, None, self)
+        schema.build_index(data, '', self)
 
     def register_index(self, index, path):
-        assert not path in self.indices
+        assert path not in self.indices
         self.indices[path] = index
 
-    def check_ref(self, ref_path, key):
-        assert key in self.indices[ref_path], "Key reference '%s' not found at index path '%s'" % (key, ref_path)
+    def check_ref(self, ref_path, key, test):
+        test.assertTrue(key in self.indices[ref_path],
+                        "Key reference '%s' not found at index path '%s'"
+                        % (key, ref_path))
 
-    def validate(self):
-        self.schema.validate(self.raw_data, self)
+    def validate(self, test):
+        self.schema.validate(self.raw_data, self, test)
 
 
 class Schema(object):
@@ -44,7 +44,7 @@ class Schema(object):
     def build_index(self, data, path, doc):
         return data
 
-    def validate(self):
+    def validate(self, data, doc, test):
         pass
 
 
@@ -52,7 +52,7 @@ class DictSchema(Schema):
     def __init__(self, items):
         super(DictSchema, self).__init__()
         self.items = items
-        key_items = filter(lambda (k,v): v.is_key, items.items())
+        key_items = filter(lambda (k, v): v.is_key, items.items())
         assert len(key_items) < 2, "Multiple fields marked as key"
         self.key = None if len(key_items) == 0 else key_items[0][0]
 
@@ -70,10 +70,10 @@ class DictSchema(Schema):
                     extend_path(k, path),
                     doc)
 
-    def validate(self, data, doc):
-        for k,schema in self.items.items():
+    def validate(self, data, doc, test):
+        for k, schema in self.items.items():
             if k in data:
-                schema.validate(data[k], doc)
+                test.validate(data[k], schema, k, doc)
 
 
 class ListSchema(Schema):
@@ -82,12 +82,11 @@ class ListSchema(Schema):
         self.item_schema = item_schema
 
     def build_index(self, data, path, doc):
-        #if hasattr(self.item_schema, 'key') and self.item_schema.key is not None:
         index = {}
         for i, item in enumerate(data):
             key = self.item_schema.get_key(item)
             if key is not None:
-                assert not key in index, "Duplicate key (%s) at path '%s'" % (key, path)
+                assert key not in index, "Duplicate key (%s) at path '%s'" % (key, path)
                 index[key] = i
                 self.item_schema.build_index(
                     item,
@@ -96,24 +95,27 @@ class ListSchema(Schema):
         if len(index) > 0:
             doc.register_index(index, path)
 
-    def validate(self, data, doc):
-        for item in data:
-            self.item_schema.validate(item, doc)
+    def validate(self, data, doc, test):
+        for i, item in enumerate(data):
+            test.validate(item, self.item_schema, i, doc)
 
 
 class IntSchema(Schema):
     def __init__(self, min=MIN_INT, max=MAX_INT, coerce=False):
         super(IntSchema, self).__init__()
-        self.min = int(min) 
-        self.max = int(max) 
+        self.min = int(min)
+        self.max = int(max)
         self.coerce = coerce
 
-    def validate(self, data, doc):
+    def validate(self, data, doc, test):
         if self.coerce:
             data = int(data)
-        assert isinstance(data, int)
-        assert data >= self.min, "Int value (%s) less than minimum (%s)" % (data, self.min)
-        assert data <= self.max
+        test.assertTrue(isinstance(data, int),
+                        "Incorrect type, expected int, found '%s'" % type(data))
+        test.assertTrue(data >= self.min,
+                        "Int value (%s) less than minimum (%s)" % (data, self.min))
+        test.assertTrue(data <= self.max,
+                        "Int value (%s) greater than maximum (%s)" % (data, self.max))
 
 
 class StrSchema(Schema):
@@ -126,10 +128,12 @@ class StrSchema(Schema):
             return data
         return None
 
-    def validate(self, data, doc):
-        assert isinstance(data, str) or isinstance(data, unicode)
+    def validate(self, data, doc, test):
+        test.assertTrue(isinstance(data, str) or isinstance(data, unicode),
+                        "Incorrect type, expected str or unicode, found '%s'"
+                        % type(data))
         if self.ref:
-            doc.check_ref(self.ref, data)
+            doc.check_ref(self.ref, data, test)
 
 
 def make_int_schema(params):
@@ -145,9 +149,9 @@ def make_str_schema(params):
 def make_value_schema(desc):
     assert isinstance(desc, str)
     value_schemas = {
-        'int' : make_int_schema,
-        'str' : make_str_schema
-    }    
+        'int': make_int_schema,
+        'str': make_str_schema
+    }
     tokens = map(str.strip, desc.split(','))
     field_type = tokens[0]
     tokens = [tok.split('=', 1) for tok in tokens[1:]]
@@ -155,43 +159,88 @@ def make_value_schema(desc):
     schema = value_schemas[field_type](params)
     schema.is_key = params.get('key', False)
     return schema
-    
+
 
 def make_schema(desc):
-    if type(desc) is types.DictType:
-        return DictSchema({k: make_schema(v) for (k,v) in desc.items()})
-    elif type(desc) is types.ListType:
+    if isinstance(desc, dict):
+        return DictSchema({k: make_schema(v) for (k, v) in desc.items()})
+    elif isinstance(desc, list):
         return ListSchema(make_schema(desc[0]))
-    elif type(desc) is types.StringType:
+    elif isinstance(desc, str) or isinstance(desc, unicode):
         return make_value_schema(desc)
 
 
+class Checker:
+    def __init__(self, reporter, path=''):
+        self.reporter = reporter
+        self.path = path
+
+    def validate(self, data, schema, key, doc):
+        schema.validate(data, doc,
+                        Checker(self.reporter,
+                                extend_path(key, self.path)))
+
+    def raiseError(self, msg):
+        self.reporter.raiseError("Error at path '%s': %s" % (self.path, msg))
+
+    def assertTrue(self, v, msg):
+        if not v:
+            self.raiseError(msg)
+
+
+class FailFastReporter:
+    def raiseError(self, msg):
+        raise AssertionError(msg)
+
+    def validate(self, doc):
+        doc.validate(Checker(self))
+
+
+class AggregateReporter:
+
+    def __init__(self):
+        self.errors = []
+
+    def raiseError(self, msg):
+        self.errors.append(AssertionError(msg))
+
+    def validate(self, doc):
+        doc.validate(Checker(self))
+        if len(self.errors) > 0:
+            for e in self.errors:
+                print e
+            raise AssertionError("Validation failed with %s errors!"
+                                 % len(self.errors))
+
 
 types_schema = make_schema({
-    "types" : ["str, key"]
+    "types": ["str, key"]
     })
 
 levels_schema = make_schema({
-     "types" : ["str, key"],
-     "levels" : [{
+     "types": ["str, key"],
+     "levels": [{
          "name": "str, key",
-         "width" : "int, min=0, max=18",
-         "height" : "int, min=0, max=25",
+         "width": "int, min=0, max=18",
+         "height": "int, min=0, max=25",
          "blocks": [{
              "type": "str, ref=types",
-             "column" : "int, min=0, max=18, coerce",
-             "row" : "int, min=0, max=25, coerce",
-             "prototype" : "str"
+             "column": "int, min=0, max=18, coerce",
+             "row": "int, min=0, max=25, coerce",
+             "prototype": "str"
          }]
      }]
 })
-    
+
 
 if __name__ == '__main__':
     with open('data.json', 'r') as f:
         data = json.load(f)
-        doc = Doc(data, levels_schema)
-        doc.validate()
-    print "Ok"
 
-    
+        try:
+            doc = Doc(data, levels_schema)
+            AggregateReporter().validate(doc)
+            # FailFastReporter().validate(doc)
+        except AssertionError, e:
+            sys.exit(e)
+    print "Ok"
